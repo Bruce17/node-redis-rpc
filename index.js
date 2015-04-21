2a5f6e500e41bbb8cdb0e1c65db7e44210325e56
Michael Raith <michael.raith@bcmsolutions.de>
2015-04-21 13:44:06 +0200

    Removed comment

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * @author Michael Raith
- * @email  michael.raith@bcmsolutions.de
- * @date   20.04.2015 17:51
- */
-
 var NodeRedisRpc = require('./lib/node-redis-rpc');
 
 module.exports = NodeRedisRpc;
